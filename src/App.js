106125893147ef26336c3eb3c5a35bec70a2a2e4
kingjs-inc <kingjsfoundation8@gmail.com>
2022-03-06 12:10:51 +0900

    [TIL 1] 클래스형 컴포넌트 To-Do 앱 만들기
      - 클래스형 컴포넌트
      - todo 데이터를 입력받아 추가 (페이지 리로드 되지 않도록)
      - 버튼으로 데이터 삭제
      - 체크박스로 데이터 완료여부(line-through) 표기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    todoData: [
+      // {
+      //   id: "1",
+      //   title: "공부하기",
+      //   completed: false,
+      // },
+      // {
+      //   id: "2",
+      //   title: "청소하기",
+      //   completed: false,
+      // },
+    ],
+    //입력하는 데이터
+    value: "",
+  };
+
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right",
+  };
+
+  getStyle = (completed) => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: completed ? "line-through" : "none",
+    };
+  };
+
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    console.log("newTodoData", newTodoData);
+    this.setState({ todoData: newTodoData });
+  };
+
+  hadleChange = (e) => {
+    // console.log("e", e.target.value);
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    // form 안에 input을 전솔할 때 페이지 리로드 되는 걸 막아줌
+    e.preventDefault();
+
+    //새로운 할 일 데이터
+    let newTodo = {
+      // id는 unique한 값이어야 하므로
+      id: Date.now(),
+      title: this.state.value,
+      completed: false,
+    };
+
+    // 원래 있던 할 일에 새로운 할 일 더해주기 - [ 기존 데이터를 넣어주고, 새로운 데이터를 업데이트해줌]
+    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
+  };
+
+  handleCompleteChange = (id) => {
+    let newTodoData = this.state.todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+
+    this.setState({ todoData: newTodoData });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="todoBlock">
+          <div className="title">
+            <h1>할 일 목록</h1>
+          </div>
+          {this.state.todoData.map((data) => (
+            <div style={this.getStyle(data.completed)} key={data.id}>
+              <input
+                type="checkbox"
+                defaultChecked={false}
+                onChange={() => this.handleCompleteChange(data.id)}
+              />
+              {data.title}
+              <button
+                style={this.btnStyle}
+                onClick={() => this.handleClick(data.id)}
+              >
+                x
+              </button>
+            </div>
+          ))}
+          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력하세요."
+              value={this.state.value}
+              onChange={this.hadleChange}
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
